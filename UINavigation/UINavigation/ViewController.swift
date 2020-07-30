37f8ac730957ff7e4f0b9d0307702b3898b62ea4
kraken808 <64954667+kraken808@users.noreply.github.com>
2020-07-30 21:26:47 +0600

    Add files via upload

---
+++ UINavigation/UINavigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UINavigation
+//
+//  Created by Murat Merekov on 17.07.2020.
+//  Copyright © 2020 Murat Merekov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
